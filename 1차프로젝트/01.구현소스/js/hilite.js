5b9362a9056220ffb3c035e7a051f2998d42e378
moonlyong <140685725+moonlyong@users.noreply.github.com>
2023-09-12 18:49:18 +0900

    dsd

--- 1차프로젝트/01.구현소스/js/hilite.js
+++ 1차프로젝트/01.구현소스/js/hilite.js
@@ -1,1 +1,25 @@
-// https://smartstore.naver.com/ckmonsters
+
+
+// 새로고침시 첫페이지로 리셋하기
+// 브라우저 스크롤바 위치캐싱
+setTimeout(() => window.scrollTo(0, 0), 500);
+
+// 2. 이벤트 등록하기
+// 대상: window
+// window.addEventListener('click',wheelFn)
+
+// 이벤트 연결하기
+
+// DOM선택함수
+const dqs = (x) => document.querySelector(x);
+const qs = (a, b) => a.querySelector(b);
+const dqsa = (x) => document.querySelectorAll(x);
+const qsa = (a, b) => a.querySelectorAll(b);
+const addevt = (ele, evt, fn) => ele.addEventListener(evt, fn);
+const clog = (x) => console.log(x);
+
+
+/******************************************** 
+            함수명 linkFn
+********************************************/
+
