e5e1d2d00f9c36f9f340d7a392af9e94034e1adc
moonlyong <140685725+moonlyong@users.noreply.github.com>
2023-09-13 19:08:44 +0900

    ssss

--- 1차프로젝트/01.구현소스/js/hilite.js
+++ 1차프로젝트/01.구현소스/js/hilite.js
@@ -1,5 +1,3 @@
-
-
 // 새로고침시 첫페이지로 리셋하기
 // 브라우저 스크롤바 위치캐싱
 // setTimeout(() => window.scrollTo(0, 0), 500);
@@ -18,8 +16,21 @@ const qsa = (a, b) => a.querySelectorAll(b);
 const addevt = (ele, evt, fn) => ele.addEventListener(evt, fn);
 const clog = (x) => console.log(x);
 
-
 /******************************************** 
             함수명 linkFn
 ********************************************/
-
+        /* 
+        <li>
+            <a href="./vodplayer.html"
+                ><img
+                    src="./이미지/프리뷰/preview.jpg"
+                    alt=""
+                />
+                <h4 class="clip-tit">
+                    [미공개] 콜드 패 위기에 굴하지 않고 슬로
+                    커브 작렬하는 유희관
+                </h4>
+                <h6 class="clip-sub">메이킹영상</h6>
+            </a>
+        </li>
+         */
