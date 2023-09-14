52307dfba38fddec54f665f165ab243ab6724262
moonlyong <140685725+moonlyong@users.noreply.github.com>
2023-09-14 18:38:53 +0900

    ss

--- 1차프로젝트/01.구현소스/js/hilite.js
+++ 1차프로젝트/01.구현소스/js/hilite.js
@@ -9,28 +9,47 @@
 // 이벤트 연결하기
 
 // DOM선택함수
-const dqs = (x) => document.querySelector(x);
-const qs = (a, b) => a.querySelector(b);
-const dqsa = (x) => document.querySelectorAll(x);
-const qsa = (a, b) => a.querySelectorAll(b);
-const addevt = (ele, evt, fn) => ele.addEventListener(evt, fn);
-const clog = (x) => console.log(x);
+const domFn ={
+    dqs: (x) => document.querySelector(x),
+    qs: (a, b) => a.querySelector(b),
+    dqsa: (x) => document.querySelectorAll(x),
+    qsa: (a, b) => a.querySelectorAll(b),
+    addevt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+    clog: (x) => console.log(x),
+} 
 
 /******************************************** 
-            함수명 linkFn
+            forin문
 ********************************************/
-        /* 
+const vodUl = domFn.dqs('.vod-ul')
+console.log(vodUl)
+
+let hcode = ""
+
+let num = 1
+
+for(let x in clipDate){
+    hcode += `
+    
+    <ul>
         <li>
-            <a href="./vodplayer.html"
+            <a href="${clipDate[x]["링크"]}"
                 ><img
-                    src="./이미지/프리뷰/preview.jpg"
+                    src="./image/preview/${x}.jpg"
                     alt=""
                 />
                 <h4 class="clip-tit">
-                    [미공개] 콜드 패 위기에 굴하지 않고 슬로
-                    커브 작렬하는 유희관
+                    ${clipDate[x]["제목"]}
                 </h4>
-                <h6 class="clip-sub">메이킹영상</h6>
+                <h6 class="clip-sub">${clipDate[x]["태그"]}</h6>
             </a>
         </li>
-         */
+    </ul>   
+
+    `
+    num++
+}
+
+// console.log(hcode)
+
+// vodUl.innerHTML = hcode
